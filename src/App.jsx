b15c212d191c462b5def717575e4dbbc2a633192
Aswathy N <aswathia.achu@gmail.com>
2024-02-04 00:45:11 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+   
+    <>
+      <div>
+      <Navbar />
     </div>
+    </>
   )
 }
 
